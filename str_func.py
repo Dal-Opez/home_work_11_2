c2cabd3f8e3bddf1e4d0b2480e6c3b0597f25d08
Alexey Grazhdankin <alex.alexxx56@gmail.com>
2023-04-02 17:47:49 +0300

    added fix for func string_upper() and string_title()

--- str_func.py
+++ str_func.py
@@ -3,8 +3,7 @@ def string_upper(input_string: str):
     Возвращает результат применения
     метода upper к аргументу типа str
     """
-    ret_val = input_string.upper()
-    return ret_val
+    return input_string.upper()
 
 
 def string_title(input_string: str):
@@ -12,5 +11,4 @@ def string_title(input_string: str):
     Применяет метод title к аргументу
     функции типа str
     """
-    ret_val = input_string.title()
-    return ret_val
+    return input_string.title()

29833661db4def5a9e97a8021073108cb6831b51
Alexey Grazhdankin <alex.alexxx56@gmail.com>
2023-04-02 16:32:17 +0300

    in str_func.py added func string_title and docstring for func string_upper()

--- str_func.py
+++ str_func.py
@@ -1,3 +1,16 @@
 def string_upper(input_string: str):
+    """
+    Возвращает результат применения
+    метода upper к аргументу типа str
+    """
     ret_val = input_string.upper()
+    return ret_val
+
+
+def string_title(input_string: str):
+    """
+    Применяет метод title к аргументу
+    функции типа str
+    """
+    ret_val = input_string.title()
     return ret_val
